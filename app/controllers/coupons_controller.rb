cf49d45e909032884d563243d1ed990ca97c0792
Aman Gupta <aman.gupta@shuttl.com>
2016-05-22 23:52:52 +0530

    adding comments

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,7 +4,7 @@ class CouponsController < ApplicationController
     coupon = Coupon.new
     coupon.coupon_type = params[:type]
     valid_upto = params[:'valid-upto']
-    if valid_upto != nil
+    if valid_upto != nil #Checking the valid-upto param.
       begin
         Date.parse(valid_upto)
       rescue ArgumentError
@@ -13,7 +13,7 @@ class CouponsController < ApplicationController
       end
       coupon.valid_upto = valid_upto
     end
-    if coupon.coupon_type == 'single-use'
+    if coupon.coupon_type == 'single-use' #coupon specific usage limit.
       coupon.usage_count = 0
       coupon.usage_limit = 1
     elsif coupon.coupon_type == 'single-use-per-user'
@@ -47,11 +47,11 @@ class CouponsController < ApplicationController
     id = params[:id]
     valid_upto = params[:'valid-upto']
     usage_limit = params[:'n']
-    if id == nil or id.to_i == 0
+    if id == nil or id.to_i == 0 # validation for id
       render json: generate_response('Id not available or Not a valid id'),status: 400
       return
     end
-    if valid_upto != nil
+    if valid_upto != nil # validation for valid upto
       begin
         Date.parse(valid_upto)
       rescue ArgumentError
@@ -59,13 +59,13 @@ class CouponsController < ApplicationController
         return
       end
     end
-    coupon = Coupon.find(id)
+    coupon = Coupon.find(id) # fetching old coupon
     if coupon == nil
       render json: generate_response('Not a valid id'),status: 400
       return
     end
     coupon.valid_upto = valid_upto
-    if coupon.coupon_type == 'multi-use' and usage_limit != nil
+    if coupon.coupon_type == 'multi-use' and usage_limit != nil # checking for usage limit
         if coupon.usage_count <= usage_limit
           coupon.usage_limit = usage_limit
         else
@@ -83,7 +83,7 @@ class CouponsController < ApplicationController
   def apply
     coupon_code = params[:coupon]
     user_id = params[:user_id]
-    if coupon_code == nil
+    if coupon_code == nil #checking a coupon code is entered or not
       render json: generate_response('please enter coupon.'),status: 400
       return
     end
@@ -92,16 +92,16 @@ class CouponsController < ApplicationController
       render json: generate_response('not a valid coupon.')
       return
     end
-    if coupon.is_delete
+    if coupon.is_delete #check if the coupon is disabled or soft deleted
       render json: generate_response('This coupon is disabled.')
       return
     end
 
-    if coupon.valid_upto != nil and coupon.valid_upto < Time.now
+    if coupon.valid_upto != nil and coupon.valid_upto < Time.now # checking for expired coupon
       render json: generate_response('coupon code is expired')
       return
     end
-    if coupon.coupon_type == 'single-use-per-user'
+    if coupon.coupon_type == 'single-use-per-user' # checking for applied coupon row if coupon is of type single use per user
       if user_id != nil
         applied_coupon = AppliedCoupon.find_by(user_id: user_id, coupon_id: coupon.id)
         if applied_coupon != nil
@@ -113,13 +113,13 @@ class CouponsController < ApplicationController
         return
       end
     end
-    if coupon.usage_limit == nil || coupon.usage_count < coupon.usage_limit
+    if coupon.usage_limit == nil || coupon.usage_count < coupon.usage_limit # checking for usage limit
       coupon.usage_count = coupon.usage_count + 1
     else
       render json: generate_response('Limit of usage for this coupon has been reached')
       return
     end
-    AppliedCouponsHelper.create(user_id,coupon.id)
+    AppliedCouponsHelper.create(user_id,coupon.id) #adding a applied coupon row to record how many time coupon is applied.
     if coupon.save
       render json: success_response_for_coupon(nil,nil)
     else
@@ -135,7 +135,7 @@ class CouponsController < ApplicationController
   def coupon_params
     params.require(:coupon).permit(:coupon_code, :coupon_type, :valid_upto, :usage_count, :usage_limit, :is_delete)
   end
-  def generate_random_coupon_code
+  def generate_random_coupon_code # function to generate a random coupon code and check that is already doesn't exist in our system
     o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
     coupon_code = (0...8).map { o[rand(o.length)] }.join
     if Coupon.find_by_coupon_code(coupon_code) != nil
